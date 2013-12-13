699b830a5a7dc4068eae226a30ba430ee45f815a
edom18 <edo.m18@gmail.com>
2013-12-13 20:29:47 +0900

    update

--- js/Class.js
+++ js/Class.js
@@ -1,66 +1,66 @@
-/* 
- * Forked by John Resig's Simple JavaScript Inheritance (http://ejohn.org/)
- * MIT Licensed.
- */
 (function (win, doc, exports, undefined) {
- 
+
     'use strict';
- 
-    var fnTest, initialize;
-    
-    fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-    initialize = false;
- 
+
+    var fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+
     function Class() { /* noop. */ }
- 
+
     Class.extend = function (props) {
- 
-        var SuperClass, prototype;
-        
-        SuperClass = this;
-        
-        initialize = true;
-        prototype = new this();
-        initialize = false;
- 
+
+        var SuperClass = this;
+
         function Class() {
-            if (initialize !== true && typeof this.init === 'function') {
+            if (typeof this.init === 'function') {
                 this.init.apply(this, arguments);
             }
         }
- 
-        for (var key in props) {
+
+        //Copy class methods.
+        util.copyClone(Class, SuperClass);
+
+        Class.prototype = Object.create(SuperClass.prototype, {
+            constructor: {
+                value: Class,
+                writable: true,
+                configurable: true
+            }
+        });
+
+        Object.keys(props).forEach(function (key) {
             var prop   = props[key],
                 _super = SuperClass.prototype[key],
                 isMethodOverride = (typeof prop === 'function' && typeof _super === 'function' && fnTest.test(prop));
- 
+
             if (isMethodOverride) {
-                prototype[key] = (function (fn, _super) {
-                    return function () {
-                        var ret,
-                            tmp = this._super;
- 
-                        this._super = _super;
- 
-                        ret = fn.apply(this, arguments);
- 
-                        this._super = tmp;
- 
-                        return ret;
-                    };
-                }(prop, _super));
+                Class.prototype[key] = function () {
+                    var ret,
+                        tmp = this._super;
+
+                    Object.defineProperty(this, '_super', {
+                        value: _super,
+                        configurable: true
+                    });
+
+                    ret = prop.apply(this, arguments);
+
+                    Object.defineProperty(this, '_super', {
+                        value: tmp,
+                        configurable: true
+                    });
+
+                    return ret;
+                };
             }
             else {
-                prototype[key] = prop;
+                Class.prototype[key] = prop;
             }
-        }
- 
-        Class.prototype = prototype;
-        Class.prototype.constructor = Class;
+        });
+
         Class.extend = SuperClass.extend;
- 
+
         return Class;
     };
- 
+
     exports.Class = Class;
 }(window, window.document, window));
